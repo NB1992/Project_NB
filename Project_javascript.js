052e337d786457be4ec002f6da87698b25c5d819
NB1992 <nial01@hotmail.com>
2015-04-24 00:05:20 +0100

    NB_DOM
    Eamon

---
+++ Project_javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	
+    //var x = document.getElementById("myRange").value;
+	//document.getElementById("demo").innerHTML = x;
+	$( "#draggable" ).draggable();
+	$( "#draggableB" ).draggable();
+	$( "#draggableC" ).draggable();
+
+	
+});
